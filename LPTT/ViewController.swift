cd5f99e23c6caf52415a9c79d85f96505124efbb
kabechan <lit.9.kabe@gmail.com>
2017-05-05 09:50:14 +0900

    Push

--- LPTT/ViewController.swift
+++ LPTT/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var countLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //背景を黒色にする
+        self.view.backgroundColor = UIColor.black
     }
 
     override func didReceiveMemoryWarning() {
